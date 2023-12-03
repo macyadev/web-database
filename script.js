00d2bef885e4cc6d50d2a93aa9681bb45f7b1efd
macyadev <2120016438@campus.ouj.ac.jp>
2023-12-03 15:28:03 +0900

    フォーム送信時のデータ処理作成

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const exportBtn = document.querySelector('.exportBtn');
 const editBtn = document.querySelector('.editBtn');
 const tbody = document.querySelector('.tbody');
 
+const APP = {
+    // 入力した内容を一次保存する配列
+    data: [],
+    init() {
+        APP.addListeners();
+    },
+    addListeners() {
+        // フォームの送信イベントを監視
+        form.addEventListener('submit', APP.createData);
+    },
+    createData(e) {
+        // エンターキーでの実行をキャンセル
+        e.preventDefault();
+        // フォームの入力値を取得
+        const formData = new FormData(form);
+        // データを保存する
+        APP.data.push(Array.from(formData.values()));
+
+        // テーブルに行を追加する
+
+        // フォームをクリアにする
+    },
+};
+
+// DOMの読み込みが完了したらAPP.initを実行
+document.addEventListener("DOMContentLoaded", APP.init);
