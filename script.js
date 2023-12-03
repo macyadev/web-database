2d59b83434434adf85fe300755f174c11724be5e
macyadev <2120016438@campus.ouj.ac.jp>
2023-12-03 15:38:49 +0900

    フォーム送信時のtrタグ追加

--- script.js
+++ script.js
@@ -22,9 +22,23 @@ const APP = {
         // データを保存する
         APP.data.push(Array.from(formData.values()));
 
-        // テーブルに行を追加する
+        // テーブルに行を追加する（タグを用意する）
+        const tr = document.createElement('tr');
+        // テーブルに行を追加する（内容を入れる）
+        tr.innerHTML += `
+            <td>${formData.get('name')}</td>
+            <td>${formData.get('address')}</td>
+            <td>${formData.get('age')}</td>
+            <td>${formData.get('tel')}</td>
+        `;
+        // テーブルに行を追加する（テーブルに追加する）
+        tbody.appendChild(tr);
 
         // フォームをクリアにする
+        form.reset();
+
+        // フォーカスを名前の入力欄に移動する
+        form.name.focus();
     },
 };
 
