17d06784a92eac9e23f0138178865c227e8636ca
macyadev <2120016438@campus.ouj.ac.jp>
2023-12-03 15:15:54 +0900

    要素の取得設定

---
+++ script.js
@@ -0,0 +1,6 @@
+// 要素の取得
+const form = document.querySelector('.form');
+const exportBtn = document.querySelector('.exportBtn');
+const editBtn = document.querySelector('.editBtn');
+const tbody = document.querySelector('.tbody');
+
